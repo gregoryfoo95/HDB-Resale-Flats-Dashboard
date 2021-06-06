35d136d0e1e157966da996b5458adc0c0d15b889
Gregory Foo <57233318+gregoryfoo95@users.noreply.github.com>
2021-06-06 18:34:26 +0800

    Update Resale_Price.py

--- Resale_Price.py
+++ Resale_Price.py
@@ -11,7 +11,7 @@ import re
 from urllib.request import Request, urlopen
 from PIL import Image
 import time
-
+from data_extraction import df
 
 
 st.set_page_config(layout="wide")
@@ -51,7 +51,7 @@ st.sidebar.header('User Input Features')
 #df = load_data()
 
     
-from data_extraction import df
+
 town = df.groupby('town')
 #subsector = df.groupby('GICS Sub-Industry')
 
