66c267d61c4ea57e6ef7eee303a8145f54eb65d7
Gregory Foo <57233318+gregoryfoo95@users.noreply.github.com>
2021-06-06 18:15:20 +0800

    Update Resale_Price.py

--- Resale_Price.py
+++ Resale_Price.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 import re
 from urllib.request import Request, urlopen
 from PIL import Image
-
+import time
 
 st.set_page_config(layout="wide")
 image = Image.open('house.png')
@@ -47,7 +47,19 @@ def load_data():
     myDataFrame = myDataFrame.append(df)
     myDataFrame.sort_values(by = ['month'],ascending = False)
     return myDataFrame
-    
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
 df = load_data()
 town = df.groupby('town')
 #subsector = df.groupby('GICS Sub-Industry')
