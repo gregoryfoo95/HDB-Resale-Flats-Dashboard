66a68a004c06f4d51208bba02538a147e2cb8788
Gregory Foo <57233318+gregoryfoo95@users.noreply.github.com>
2021-06-06 20:47:46 +0800

    Update Resale_Price.py

--- Resale_Price.py
+++ Resale_Price.py
@@ -98,14 +98,9 @@ st.markdown(filedownload(df_selected_town), unsafe_allow_html=True)
 
 # Plot Number of flat types for estates
 def type_plot(town):
-    fig = plt.figure()
-    ax = fig.add_axes([0,0,1,1])
-    x = df_selected_town['flat_type']
-    y_temp = df_selected_town['flat_type'].unique().to_list()
-    for i in y_temp:
-        y[i] = df_selected_town['flat_type'][i].counts()
-    ax.bar(x,y)
-    return st.pyplot(fig)
+    town.plot.bar(x="town", y="flat_type", rot=70, title="Number of tourist visits - Year 2018");
+
+plot.show(block=True);
 # Plot Price of Resale Price
 def price_plot(town):
 #  df = pd.DataFrame(data[symbol].Close)
